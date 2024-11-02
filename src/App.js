1a1881e7cf90e5387d3bf857cd618d048acb2321
Raniyaaaa <raniya182002@gmail.com>
2024-11-02 23:42:18 +0530

    E commerce website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { Button, Container, Navbar,Badge,Row,Col,Image} from 'react-bootstrap';
 import './App.css';
+import Product from './components/Products/Product';
 
-function App() {
+const App=()=> {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <Navbar bg="black" expand="sm" variant="dark" style={{paddingRight:'1rem' ,fontFamily: 'Times New Roman'}}>
+        <Container className="justify-content-center">
+          <Navbar.Brand href="#home" >HOME</Navbar.Brand>{' '}
+          <Navbar.Brand href="#store">STORE</Navbar.Brand>{' '}
+          <Navbar.Brand href="#about">ABOUT</Navbar.Brand>{' '}
+        </Container>
+        <Button variant='outline-info'>Cart</Button>{' '}
+        <Badge
+            bg="none"
+            style={{
+              color:'skyblue',
+              position: 'absolute',
+              top: '-6px',      // Adjust the positioning as needed
+              right: '-1px',    // Align to the top-right corner
+              fontSize: '1.2rem',
+              padding: '0.4rem',
+            }}>
+            {0}
+          </Badge>
+      </Navbar>
+      <header className="mt-1" style={{background:"grey"}}>
+        <Container className="justify-content-center" style={{paddingBottom:'3rem'}}>
+          <h1 style={{ fontSize: '5rem', color: 'white', fontWeight:'bold' ,textAlign: 'center' ,fontFamily: 'Times New Roman'}}>The Generics</h1>
+        </Container>
       </header>
-    </div>
+      <Product/>
+      <footer style={{ background: "skyblue", padding: '1rem' }}>
+        <Container className="d-flex justify-content-between align-items-center">
+          <h3 style={{ fontSize: '3rem',color:'white', fontWeight: 'bold', fontFamily: 'Times New Roman'}}>The Generics</h3>
+          <div>
+            <a href="https://www.youtube.com/"><Image src="https://prasadyash2411.github.io/ecom-website/img/6260efc8fc9a9002669d2f4ad9956cc0.jpg" thumbnail style={{ width: '40px', height: '40px' ,background:'skyblue',border:'none' }} /></a>
+            <a href="https://open.spotify.com/"><Image  src="https://static.vecteezy.com/system/resources/previews/023/986/728/non_2x/spotify-logo-spotify-logo-transparent-spotify-icon-transparent-free-free-png.png" thumbnail style={{ width: '40px', height: '40px', marginRight: '0.5rem',background:'skyblue',border:'none' }} /></a>
+            <a href="https://www.facebook.com/"><Image  src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png" thumbnail style={{ width: '40px', height: '40px', marginRight: '0.5rem' ,background:'skyblue',border:'none' }} /></a>
+          </div>
+        </Container>
+      </footer>
+    </>
   );
 }
 
---
+++ src/components/Products/Product.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { Container,Button, Row,Col,Card } from "react-bootstrap"
+const Product=()=>{
+
+    const productsArr = [
+        { title: 'Colors', price: 100, imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png'},
+        { title: 'Black and white Colors', price: 50, imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png'},
+        { title: 'Yellow and Black Colors', price: 70,imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png'},
+        { title: 'Blue Color', price: 100, imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png'}]
+        
+        return(
+            <>
+                <Container>
+                    <h1 style={{ fontFamily: 'fantasy', textAlign: 'center',paddingTop:'2rem'}}>MUSIC</h1>
+                    <Row style={{marginRight: '10rem' ,marginLeft: '10rem' }}>
+                        {productsArr.map((product, index) => (
+                        <Col md={6} key={index} className="justify-content-center">
+                            <Card style={{ textAlign: 'center',fontFamily: 'Times New Roman',scale:'80%',border:'none'}}>
+                                <Card.Title >{product.title}</Card.Title>
+                                <Card.Img variant="none" 
+                                src={product.imageUrl} 
+                                alt={product.title} 
+                                style={{padding:"2rem",
+                                transition: "transform 0.5s",
+                                objectFit:"inherit"
+                                }}
+                                onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+                                onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+                                />                            
+                                <Card.Body className="d-flex justify-content-between align-items-center">
+                                <Card.Text>${product.price}</Card.Text>
+                                    <Button variant="info" style={{color:'white',borderRadius:'0'}}>ADD TO CART</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        ))}
+                    </Row>
+                    <div className="d-flex justify-content-center m-3">
+                        <Button variant="secondary" style={{color:"skyblue"}}>See the Cart</Button>
+                    </div>
+                </Container>
+            </>
+        )
+}
+
+export default Product;
+ 
